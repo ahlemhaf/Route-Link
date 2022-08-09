0456eafd5759dcb3235ff6faf690b60b0ddef6cc
ahlemhaf <ahlemhafsouni044@gmail.com>
2022-08-09 16:17:15 +0200

    Route Link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Menu from './components/Menu';
+import Docs from './components/Docs';
+import Tutorials from './components/Tutorials';
+import Community from './components/Community';
+import ErrorPage from './components/ErrorPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route exact path="/" element={<Docs />}/>
+        <Route path="/community" element={<Community/>}/>
+        <Route  path="/tutorials"element={<Tutorials/>} />
+        <Route path ="*"element={<ErrorPage/>} />
+
+      </Routes>
+     
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div className='container'>
+      <h1>PAGE NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default ErrorPage
